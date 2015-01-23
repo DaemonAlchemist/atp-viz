94c7dfd4c28458489f39eb765ea0063aebc27808
Power System Engineering <info@powersystem.org>
2015-01-23 16:59:51 -0600

    Remove unneeded getClasses function

--- src/ATPViz/Widget/DataTable.php
+++ src/ATPViz/Widget/DataTable.php
@@ -11,26 +11,6 @@ class DataTable extends \ATPViz\Widget\AbstractWidget
 		$this->setTemplate('atp-viz/widget/dataTable.phtml');
 	}
 
-	public function getClasses($row)
-	{
-		$classes = array();
-		if(isset($this->classFields))
-		{
-			foreach($this->classFields as $field)
-			{
-				$value = $row[$field];
-				$method = "get{$field}Class";
-				if(method_exists($this, $method)) $value = $this->$method($value);
-				
-				$class = str_replace(array("/", " "), "-", trim("{$field}-{$value}"));
-				while(strpos($class, "--") !== false) $class = str_replace("--", "-", $class);
-				$classes[] = $class;
-			}
-		}
-		
-		return implode(" ", $classes);
-	}
-	
 	public function getSampleData()
 	{
 		return array(
